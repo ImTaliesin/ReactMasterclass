f29a43ea1fbc702771504fc326209983134b6f8b
ImTaliesin <brennanjdouglas@gmail.com>
2023-07-23 21:45:10 -0700

    Learned a lot

--- Test1/src/index.js
+++ Test1/src/index.js
@@ -1,11 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const name = "Talie"
-const luckyNum = 9
+const img = "https://picsum.photos/200"
+const img2 = "https://picsum.photos/200/200"
+const img3 = "https://picsum.photos/200/201"
+let customStyle = {
+  color: "teal",
+  fontSize: "20px",
+  border: "1px solid black"
+}
+const currentYear = new Date().getFullYear();
 ReactDOM.render(
     <div>
-        <h1>Hello {name}, your lucky number is {luckyNum}</h1>
+      <h1 style={customStyle} contentEditable='true' spellCheck='false'>My Favorite Places</h1>
+      <img src={img} alt="random image" className="img"></img>
+      <img src={img2} alt="random image" className="img"></img>
+      <img src={img3} alt="random image" className="img"></img>
     </div>
     ,
     document.getElementById('root'));
